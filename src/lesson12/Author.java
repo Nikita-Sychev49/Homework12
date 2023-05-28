d2808fa48a9247a328f38003011005e26add0261
Nikita-Sychev <komap3965@icloud.com>
2023-05-28 18:22:38 +0300

    Домашнее задание по методам объектов сделано

--- src/lesson12/Author.java
+++ src/lesson12/Author.java
@@ -1,8 +1,10 @@
 package lesson12;
 
+import java.util.Objects;
+
 public class Author {
-    private String firstName;
-    private String middleName;
+    private final String firstName;
+    private final String middleName;
 
     public Author(String firstName, String middleName) {
         this.firstName = firstName;
@@ -15,5 +17,23 @@ public class Author {
 
     public String getMiddleNameName() {
         return this.middleName;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + middleName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(firstName, author.firstName) && Objects.equals(middleName, author.middleName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, middleName);
     }
 }
